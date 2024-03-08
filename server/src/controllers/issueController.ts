8bea2fa25a04d5eaa8a9f4786ca7c9efefdd0f6c
Han Xiao <asher.han.xiao@gmail.com>
2024-03-08 17:06:15 +1100

    feat: get issue api

--- server/src/controllers/issueController.ts
+++ server/src/controllers/issueController.ts
@@ -1,12 +1,38 @@
 import { Request, Response } from "express";
 import Issue from "../models/issue";
 
+/**
+ * Get issue by id
+ */
+const getIssue = async (req: Request, res: Response) => {
+  try {
+    // 1. get issue by id
+    const { id } = req.body;
+    const issue = await Issue.findOne({ id: id });
+
+    // 1.1 return error if not exsist
+    if (!issue) {
+      return res.status(404).json({ message: "Issue not found!" });
+    }
+
+    // 2. return issue
+    res.json(issue);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error getting issue" });
+  }
+};
+
+/**
+ * Create a new issue
+ */
 const createIssue = async (req: Request, res: Response) => {
   try {
     // 1. check if the issue id exists
     const { id } = req.body;
     const exsistingIssue = await Issue.findOne({ id: id });
 
+    // 1.1 return error if not exsist
     if (exsistingIssue) {
       return res.status(403).json({ message: "Issue id already exsist" });
     }
@@ -16,7 +42,7 @@ const createIssue = async (req: Request, res: Response) => {
     await newIssue.save();
 
     // 3. return the issue object to the calling client
-    res.status(201).json(newIssue.toObject());
+    res.status(201).json(newIssue);
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Error creating issue" });
@@ -24,5 +50,6 @@ const createIssue = async (req: Request, res: Response) => {
 };
 
 export default {
+  getIssue,
   createIssue,
 };
--- server/src/routes/issueRoute.ts
+++ server/src/routes/issueRoute.ts
@@ -3,6 +3,9 @@ import issueController from "../controllers/issueController";
 
 const router = express.Router();
 
+// get issue by id
+router.get("/", issueController.getIssue);
+
 // create new issue if provided id does not exist
 router.post("/", issueController.createIssue);
 
